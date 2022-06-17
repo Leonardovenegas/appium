113f377c803a00fafd59adbf679c885eeccb7793
Leonardo Venegas <lvenegas.connectis@banchochile.cl>
2022-06-17 10:38:09 -0400

    comparaciones de datos

--- src/test/java/testClasse/OperacionesMatematicas.java
+++ src/test/java/testClasse/OperacionesMatematicas.java
@@ -17,15 +17,32 @@ public class OperacionesMatematicas {
     public void sumar(String nros){
         PdfQaNovaReports.addWebReportImage("Apertura Calculadora", "Se abre la aplicacion calculadora", EstadoPrueba.DONE, false);
         calculadora = new Calculadora();
+        int suma = 0;
+        String calculo = "";
         List<String> numeros = Arrays.asList(nros.split(","));
         for (int x = 0; x < numeros.size(); x++){
             calculadora.ingresarNumeros(numeros.get(x));
+            PdfQaNovaReports.addWebReportImage("Ingreso numero "+(x+1), "Se ingresa numero " + (x+1) + ", el cual es " + numeros.get(x), EstadoPrueba.DONE, false);
+            calculo = calculo + numeros.get(x).replace(".", "") + "+";
+            suma = suma + Integer.parseInt(numeros.get(x));
             calculadora.sumar();
         }
-        PdfQaNovaReports.addWebReportImage("Operacion matematica", "La operacion de suma es la siguiente.", EstadoPrueba.DONE, false);
-        System.out.println("La operacion matematica es: " + calculadora.obtenerResultado().substring(0, calculadora.obtenerResultado().length() - 1));
+        calculo = calculo.substring(0, calculo.length() - 1);
+        String calculoTelefono = calculadora.obtenerResultado().replace(".", "");
+        calculoTelefono = calculoTelefono.substring(0, calculoTelefono.length() - 1);
+        if (calculo.equals(calculoTelefono)){
+            PdfQaNovaReports.addWebReportImage("Operacion matematica.", "La operacion matematica ingresada es igual a la entregada, " + calculo, EstadoPrueba.PASSED, false);
+        } else {
+            PdfQaNovaReports.addWebReportImage("Operacion matematica.", "La operacion matematica ingresada es diferente a la entregada, operacion entregada " + calculo + ", operacion ingresada " + calculoTelefono, EstadoPrueba.FAILED, false);
+        }
+        System.out.println("La operacion matematica es: " + calculoTelefono);
         calculadora.igual();
-        PdfQaNovaReports.addWebReportImage("Resultado", "El resultado es.", EstadoPrueba.DONE, false);
-        System.out.println("El resultado es: " + calculadora.obtenerResultado());
+        int sumaTelefono = Integer.parseInt(calculadora.obtenerResultado().replace(".", ""));
+        if (sumaTelefono == suma){
+            PdfQaNovaReports.addWebReportImage("Resultado.", "El resultado es el correcto, " + suma, EstadoPrueba.PASSED, false);
+        } else {
+            PdfQaNovaReports.addWebReportImage("Resultado.", "El resultado es incorrecto, resultado " + suma + ", resultado calculadora " + sumaTelefono, EstadoPrueba.FAILED, false);
+        }
+        System.out.println("El resultado es: " + sumaTelefono);
     }
 }
